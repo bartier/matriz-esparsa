57bb00b4e692881503de77134e29d4491960e916
Vitor Bartier <vitorbartier@hotmail.com>
2017-04-08 20:39:14 -0300

    Adicionado documentação na Célula
    - Métodos documentados;
    - Valor pode ser nulo (útil para células cabeça);
    - Código melhorado.

--- MatrizEsparsa/MatrizEsparsa/Celula.cs
+++ MatrizEsparsa/MatrizEsparsa/Celula.cs
@@ -8,87 +8,77 @@ namespace MatrizEsparsa
 {
     class Celula
     {
-        private double valor;
+        private double? valor;
         private int linha, coluna;
         private Celula direita, abaixo;
 
-        public Celula(double valor, int linha, int coluna)
+        /// <summary>
+        /// Cria uma célula contendo um valor numa determinada coordenada de linha/coluna.
+        /// </summary>
+        /// <param name="valor"></param>
+        /// <param name="linha"></param>
+        /// <param name="coluna"></param>
+        public Celula(double? valor, int linha, int coluna)
         {
             Valor = valor;
             Linha = linha;
             Coluna = coluna;
         }
 
-        public double Valor
+        /// <summary>
+        /// O valor que a célula guarda.
+        /// </summary>
+        public double? Valor
         {
-            get
-            {
-                return valor;
-            }
-
-            set
-            {
-                valor = value;
-            }
+            get { return valor; }
+            set { valor = value; }
         }
 
+        /// <summary>
+        /// A linha que a célula se encontra.
+        /// </summary>
         public int Linha
         {
-            get
-            {
-                return linha;
-            }
-
-            set
-            {
-                linha = value;
-            }
+            get { return linha; }
+            set { linha = value; }
         }
 
+        /// <summary>
+        /// A coluna que a célula se encontra.
+        /// </summary>
         public int Coluna
         {
-            get
-            {
-                return coluna;
-            }
-
-            set
-            {
-                coluna = value;
-            }
+            get { return coluna; }
+            set { coluna = value; }
         }
 
+        /// <summary>
+        /// Acesso a celula da direita.
+        /// </summary>
         public Celula Direita
         {
-            get
-            {
-                return direita;
-            }
-
-            set
-            {
-                direita = value;
-            }
+            get {  return direita; }
+            set { direita = value; }
         }
 
+        /// <summary>
+        /// Acesso a celula debaixo.
+        /// </summary>
         public Celula Abaixo
         {
-            get
-            {
-                return abaixo;
-            }
-
-            set
-            {
-                abaixo = value;
-            }
+            get { return abaixo; }
+            set { abaixo = value; }
         }
 
+        /// <summary>
+        /// Representa a célula na string: (valor, linha, coluna).
+        /// </summary>
+        /// <returns>(valor, linha, coluna)</returns>
         public override string ToString()
         {
             String ret = "(";
 
-            ret += this.Valor + ";" + this.Linha + ";" + this.Coluna;
+            ret += (this.Valor==null? (double?)0 : this.Valor) + ";" + this.Linha + ";" + this.Coluna;
 
             return ret + ")";
         }
